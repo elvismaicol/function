ecc6bce1f9c56b1418ca7972e77d75c5df46b55d
elvismaicol <elvismaicol@gmail.com>
2020-02-05 21:53:59 -0300

    6ª Parte - Expressão lambda inline-Correção

--- src/application/Program.java
+++ src/application/Program.java
@@ -4,7 +4,6 @@ import entities.Product;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
-import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class Program {
